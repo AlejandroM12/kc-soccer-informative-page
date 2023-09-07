b003765f1c2607cc43e17233670e8f00c573e94b
alejandro <alejandro.morel1905@gmail.com>
2023-09-07 12:20:12 -0300

    feat: add hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import './Hero.css';
+
+const Hero = () => {
+  return (
+    <div className='Hero'>
+      <div className='secContainer'>
+        <div className='textDiv'>
+          <h1>KC Soccer FIFA AGENT</h1>
+          <p>
+            Somos una empresa de representación de deportistas, con mas de 20
+            años de experiencia. Avalados con Licencia FIFA, para asesorar
+            futbolistas.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
