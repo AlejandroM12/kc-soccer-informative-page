afe0ce4b899bbfb4993d6e187c7b0e68d9edce70
alejandro <alejandro.morel1905@gmail.com>
2023-09-07 13:46:09 -0300

    fix(navbar): toggle menu on button click and close when clicked outside
    - Added stopPropagation to prevent click event conflicts.
    - Made the menu close properly when clicked outside.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
+import { useState, useEffect, useRef } from 'react';
 import logo from '@assets/logo-n-bg.png';
-import { useState } from 'react';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { PiDotsNineBold, PiInstagramLogoLight } from 'react-icons/pi';
 import './Navbar.css';
@@ -7,13 +7,30 @@ import './Navbar.css';
 const Navbar = () => {
   const [menu, setMenu] = useState('menu');
   const [transparent, setTransparent] = useState('Navbar');
+  const menuRef = useRef(null);
 
-  const showNavbar = () => {
-    setMenu('showMenu menu');
+  const toggleMenu = (e) => {
+    e.stopPropagation();
+    if (menu === 'menu') {
+      setMenu('showMenu menu');
+    } else {
+      setMenu('menu');
+    }
   };
-  const removeNavbar = () => {
-    setMenu('menu');
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setMenu('menu');
+    }
   };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClickOutside);
+    return () => {
+      window.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
   const addBg = () => {
     if (window.scrollY >= 10) {
       setTransparent('Navbar addBackground');
@@ -21,24 +38,30 @@ const Navbar = () => {
       setTransparent('Navbar');
     }
   };
-  window.addEventListener('scroll', addBg);
+
+  useEffect(() => {
+    window.addEventListener('scroll', addBg);
+    return () => {
+      window.removeEventListener('scroll', addBg);
+    };
+  }, []);
+
   return (
     <div className={transparent}>
       <div className='logoDiv'>
         <img src={logo} alt='logo' />
       </div>
-      <div className={menu}>
+      <div className={menu} ref={menuRef}>
         <ul>
           <li className='navList'>Profesionales</li>
           <li className='navList'>Quiénes Somos</li>
           <li className='navList'>Contacto</li>
         </ul>
-        <AiFillCloseCircle className='icon closeIcon' onClick={removeNavbar} />
+        <AiFillCloseCircle className='icon closeIcon' onClick={toggleMenu} />
       </div>
 
       <PiInstagramLogoLight className='icon instagram' />
-
-      <PiDotsNineBold className='icon menuIcon' onClick={showNavbar} />
+      <PiDotsNineBold className='icon menuIcon' onClick={toggleMenu} />
     </div>
   );
 };
